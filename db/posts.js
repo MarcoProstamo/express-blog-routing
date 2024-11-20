cba889bb7e41580cdf9bbdcb45db86b26a51b188
MarcoProstamo <Marcopros1201@gmail.com>
2024-11-20 15:04:25 +0100

    Feat → Added CRUD Operations

--- db/posts.js
+++ db/posts.js
@@ -1,29 +1,34 @@
 const posts = [
   {
+    id: 1,
     titolo: "Post 1",
     contenuto: "Lorem Ipsum",
     immagine: "./public/images/ciambellone.jpeg",
     tags: ["Sweet", "Soft", "Light"],
   },
   {
+    id: 12,
     titolo: "Post 2",
     contenuto: "Lorem Ipsum",
     immagine: "./public/images/cracker_barbabietola.jpeg",
     tags: ["Sweet", "Hard", "Light", "Small"],
   },
   {
+    id: 8,
     titolo: "Post 3",
     contenuto: "Lorem Ipsum",
     immagine: "./public/images/pane_fritto_dolce.jpeg",
     tags: ["Sweet", "Hard", "Heavy"],
   },
   {
+    id: 4,
     titolo: "Post 4",
     contenuto: "Lorem Ipsum",
     immagine: "./public/images/pasta_barbavietola.jpeg",
     tags: ["Heavy"],
   },
   {
+    id: 3,
     titolo: "Post 5",
     contenuto: "Lorem Ipsum",
     immagine: "./public/images/torta_paesana.jpeg",
--- router/posts.js
+++ router/posts.js
@@ -6,7 +6,81 @@ const router = express.Router();
 
 // Index
 router.get("/", (req, res) => {
-  res.send("Show all Posts");
+  res.json(posts);
+});
+
+// Show
+router.get("/:id", (req, res) => {
+  id = parseInt(req.params.id);
+  if (isNaN(id)) {
+    res.status(406).json({ error: "Id Not Valid" });
+    return;
+  }
+  const selectedPost = posts.find((post) => post.id === id);
+  if (!selectedPost) {
+    res.status(404).json({ error: "Post Not Found" });
+    return;
+  }
+  res.json(selectedPost);
+});
+
+// Create
+router.post("/", (req, res) => {
+  res.json("Post Creato");
+});
+
+// Update
+router.put("/:id", (req, res) => {
+  id = parseInt(req.params.id);
+  if (isNaN(id)) {
+    res.status(406).json({ error: "Id Not Valid" });
+    return;
+  }
+  const selectedPost = posts.find((post) => post.id === id);
+  if (!selectedPost) {
+    res.status(404).json({ error: "Post Not Found" });
+    return;
+  }
+  res.json(`Post con ID: ${id} → Sostituito`);
+});
+
+// Modify
+router.patch("/:id", (req, res) => {
+  id = parseInt(req.params.id);
+  if (isNaN(id)) {
+    res.status(406).json({ error: "Id Not Valid" });
+    return;
+  }
+  const selectedPost = posts.find((post) => post.id === id);
+  if (!selectedPost) {
+    res.status(404).json({ error: "Post Not Found" });
+    return;
+  }
+  res.json(`Post con ID: ${id} → Modificato`);
+});
+
+// Destroy
+router.delete("/:id", (req, res) => {
+  id = parseInt(req.params.id);
+  if (isNaN(id)) {
+    res.status(406).json({ error: "Id Not Valid" });
+    return;
+  }
+
+  let selectedPostIndex;
+  posts.find((post, index) => {
+    if (post.id === id) selectedPostIndex = index;
+  });
+
+  if (!posts.find((post) => post.id === id)) {
+    res.status(404).json({ error: "Post Not Found" });
+    return;
+  }
+
+  const postEliminato = posts[selectedPostIndex];
+  posts.slice(selectedPostIndex, 1);
+
+  res.json({ postEliminato, posts });
 });
 
 module.exports = router;
